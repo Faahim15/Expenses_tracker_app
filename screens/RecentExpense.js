f4a0af562f55b4cc6f9cf2520ca07de841a67b3a
Faahim15 <abdulkaderfahim6@gmail.com>
2025-08-16 17:45:31 +0600

    http request practised using firebase

--- screens/RecentExpense.js
+++ screens/RecentExpense.js
@@ -1,10 +1,19 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/expensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../store/http";
 export default function RecentExpense() {
-  const expensesCtx = useContext(ExpensesContext);
-  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+  // const expensesCtx = useContext(ExpensesContext);
+  const [fetchedExpenses, setFetchedExpenses] = useState([]);
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+      setFetchedExpenses(expenses);
+    }
+    getExpenses();
+  }, []);
+  const recentExpenses = fetchedExpenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
     return expense.date >= date7DaysAgo && expense.date <= today; // ✅ ensure proper comparison
